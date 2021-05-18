6c6d54eba0a4a1e1a48802c6be21b580ff5a50e5
Elizaahmad <74451187+Elizaahmad@users.noreply.github.com>
2021-05-18 12:38:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function preload(){
+ClownFilter=loadImage('https://i.postimg.cc/7ZT7HGFM/birds-png-3502.png');
+} 
+
+function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+video=createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+poseNet = ml5.poseNet(video,modelloaded);
+poseNet.on('pose',gotposes);
+}
+
+function draw(){
+image(video,0,0,300,300);
+image(ClownFilter,noseX,noseY,150,150);
+
+}
+
+function modelloaded(){
+    console.log('modelloaded');
+}
+noseX=0;
+noseY=0;
+function gotposes(results){
+    if(results.length>0)
+    {
+        noseX=results[0].pose.nose.x- 150;
+        noseY=results[0].pose.nose.y- 45;
+    }
+}
+function Take_Snapshot(){
+    save('Selfie.png');
+}
